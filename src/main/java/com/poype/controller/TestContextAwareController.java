a5f04f4727543d8e9f26c37ab90af56dc7a063d7
poype <253186926@qq.com>
2023-10-27 09:05:04 +0800

    【Spring】测试ApplicationContextAware

---
+++ src/main/java/com/poype/controller/TestContextAwareController.java
@@ -0,0 +1,25 @@
+package com.poype.controller;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestContextAwareController implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @GetMapping("/test_context_aware")
+    public String test() {
+        // 从IOC容器中获取HelloController Bean
+        HelloController helloController = this.applicationContext.getBean(HelloController.class);
+        return helloController.hello();
+    }
+}
