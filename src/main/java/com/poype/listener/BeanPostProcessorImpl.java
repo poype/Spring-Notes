86e4b31e098eff857cebcf3bb85bffff75bea7c2
poype <253186926@qq.com>
2023-10-29 12:37:45 +0800

    【Spring】 BeanPostProcessor 容器中所有bean的实例化都可以监控到

---
+++ src/main/java/com/poype/listener/BeanPostProcessorImpl.java
@@ -0,0 +1,19 @@
+package com.poype.listener;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanPostProcessorImpl implements BeanPostProcessor {
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization " + beanName);
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization " + beanName);
+        return bean;
+    }
+}
